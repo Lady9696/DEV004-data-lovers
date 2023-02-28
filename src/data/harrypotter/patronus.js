04de7dc106081f19a65e00ddf5fbf45cf4e9d7e9
Leidy Suarez Miranda <leidysuarez55@gmail.com>
2023-02-28 14:33:29 -0500

    Final!1

--- src/data/harrypotter/patronus.js
+++ src/data/harrypotter/patronus.js
@@ -3,9 +3,9 @@ export const data2 = {
         {
             "id": 50,
             "name": "Sirius Black",
-            "patronus": "Hare",
-            "description": "Una liebre, igual a su personalidad extrovertida, curiosa, saltarina.Su Patronus muestra que está orgullosa de ser ella misma, sin importar lo que digan los demás.",
-            "img2": "https://vignette.wikia.nocookie.net/harrypotter/images/f/f8/Patronus_Hare.jpg/revision/latest?cb=20170109002548",
+            "patronus": "Dog",
+            "description": "El patronus de este animago, es el mismo que su animal de transformación.",
+            "img2": "https://pm1.narvii.com/7088/03b50d4b9b91f83812f230300ce1664151debe78r1-968-792v2_hq.jpg",
             "power": 89,
             "img": "https://elcomercio.pe/resizer/Px9iGIISx0qPXMbaWvArKkvGDmI=/580x330/smart/filters:format(jpeg):quality(75)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/HC57PGWU7VAG5JG3JL7ASB2V2E.jpg",
         }
@@ -14,7 +14,7 @@ export const data2 = {
             "id": 326,
             "name": "Harry Potter",
             "patronus": "Stag",
-            "description":"Un ciervo con astas, en representación de su padre James Potter el cual podía transformarse a voluntad en uno de estos hermosos animales. Por eso James se manifiesta cuando su hijo mas lo necesita",
+            "description":"Un ciervo con astas, representa a su padre James el cual podía transformarse a voluntad en uno de estos animales. Por eso James se manifiesta cuando su hijo más lo necesita.",
             "img2": "https://vignette.wikia.nocookie.net/harrypotter/images/f/fe/Patronus_PM_SilverStagPatronus_MomentIllust.jpg/revision/latest?cb=20170314002945",
             "power": 90,
             "img": "https://www.bloghogwarts.com/wp-content/uploads/2011/02/harry-potter.jpg"
@@ -25,7 +25,7 @@ export const data2 = {
             "id": 258,
             "name": "Remus Lupin",
             "patronus": "Wolf",
-            "description":"Un lobo, pero por miedo a que se descubra su condición no le gusta mostrarlo",
+            "description":"Un lobo, pero por miedo a que se descubra su condición no le gusta mostrarlo.",
             "img2": "https://pm1.narvii.com/6253/0ac78ae011a1b4dc01e4cb27fd868e174cbe992d_hq.jpg",
             "power": 85,
             "img": "https://i.pinimg.com/564x/73/f8/51/73f85175c304bbb2298db1e1a729e591.jpg"
@@ -34,7 +34,7 @@ export const data2 = {
             "id": 382,
             "name": "Severus Snape",
             "patronus": "Doe",
-            "description":"El simbolismo más lindo y trágico de Harry Potter. El patronus de lily es el femenino del de James mientras que el de Snape es el mismo que el de Lily, al ser su eterno amor.",
+            "description":"El simbolismo más lindo y trágico de Harry Potter. El patronus de lily es el femenino del de James, mientras que el de Snape es el mismo que el de Lily, al ser su eterno amor.",
             "img2": "https://images.ctfassets.net/usf1vwtuqyxm/4UY6uN1NXOgIyO8suQGqUM/ec7de1c7b8be957cd729b2f57a880400/7.19.1_SilverDoe_Colour_01VG.jpg?w=914",
             "power": 92,
             "img": "https://i.blogs.es/8319cd/snapea/840_560.jpg"
@@ -43,7 +43,7 @@ export const data2 = {
             "id": 469,
             "name": "Arthur Weasley",
             "patronus": "Weasel",
-            "description":"Una comadreja. Tal vez se deba a que Weasley (su apellido) sea muy, muy parecido a Weasel que es comadreja en inglés",
+            "description":"Una comadreja. Tal vez se deba a que Weasley (su apellido) sea muy, muy parecido a Weasel que es comadreja en inglés.",
             "img2": "https://i.pinimg.com/originals/c4/b4/bb/c4b4bb73a5b9fccabe804f2c6de23eb2.jpg",
             "power": 55,
             "img": "https://media.harrypotterfanzone.com/arthur-weasley-half-blood-prince-portrait-1050x0-c-default.jpg"
@@ -52,8 +52,8 @@ export const data2 = {
             "id": 476,
             "name": "Ronald Weasley",
             "patronus": "Jack Russell terrier",
-            "description":"Los terriers son perros pequeños que sobreestiman seriamente su propio tamaño y fuerza.Asimismo, es un signo infalible de un amigo leal",
-            "img2": "https://es.web.img2.acsta.net/r_1920_1080/newsv7/16/09/24/13/59/3239940.jpg",
+            "description":"Los terriers son perros pequeños que sobreestiman seriamente su propio tamaño y fuerza.Asimismo, es un signo infalible de un amigo leal.",
+            "img2": "https://www.planetahobby.es/wp-content/uploads/2021/03/Figura-Patronus-Ron-Weasley-Harry-Potter.jpg",
             "power": 64,
             "img": "https://cdn-e360.s3-sa-east-1.amazonaws.com/por-que-ron-weasley-habla-parsel-en-harry-potter-large-AMcieShYPV.jpg"
         },
@@ -71,7 +71,7 @@ export const data2 = {
             "id": 587,
             "name": "Hermione Granger",
             "patronus": "Otter",
-            "description":"Una nutria, animal conocido por su inteligencia y tenacidad. Aparece en la quinta película y quedó confirmado como canónic por declaraciones de J. K. Rowling",
+            "description":"Una nutria, animal conocido por su inteligencia y tenacidad. Aparece en la quinta película y quedó confirmado como canónic por declaraciones de J. K. Rowling.",
             "img2": "https://vignette.wikia.nocookie.net/harrypotter/images/f/f3/Hermione_Otter_Patronus.png/revision/latest?cb=20170109003508",
             "power": 87,
             "img": "https://www.bloghogwarts.com/wp-content/uploads/2007/10/hermione-granger.jpg"
@@ -98,7 +98,7 @@ export const data2 = {
             "id": 727,
             "name": "Dolores Umbridge",
             "patronus": "Persian cat",
-            "descripción": "Dolores cuenta con una  una afición a los gatos, así como pudo verse en las películas en las cuales apareció",
+            "description": "Dolores cuenta con una  una afición a los gatos, así como pudo verse en las películas en las cuales apareción.",
             "img2": "https://i0.statig.com.br/bancodeimagens/dk/3r/cp/dk3rcpdzmjkgbkitihhqoghi0.jpg",            
             "power": 66,
             "img": "https://media.harrypotterfanzone.com/dolores-umbridge-order-of-the-phoenix-portrait-7.jpg"
